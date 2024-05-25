c3d1b8586ea1c1ad45dd2ebc3c5f3b704321eab5
Amine Fadssi <amin.fadssi@gmail.com>
2024-05-25 18:13:41 +0100

    first commit

---
+++ src/test/java/com/example/microbanksoapclient/MicroBankSoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microbanksoapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBankSoapClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
